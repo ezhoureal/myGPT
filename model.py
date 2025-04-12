a8eb869e9981a0d02236b16eb4cfa41eb3705309
Tianer Zhou <ezhoureal@gmail.com>
2025-04-12 20:05:04 +0800

    inference
    Signed-off-by: Tianer Zhou <ezhoureal@gmail.com>

--- model.py
+++ model.py
@@ -118,4 +118,33 @@ class GPT(nn.Module):
 
 # using a fixed config for now
 model = GPT.from_pretrained()
-model.forward(torch.arange(0, 5).view(5, 1))
+
+BATCH = 5
+MAX_LEN = 30
+K = 50
+PROMPT = "Hello, LLM. What's your name?"
+
+# inference
+import tiktoken, tqdm
+
+enc = tiktoken.encoding_for_model('gpt2')
+tokens = enc.encode(PROMPT)
+PROMPT_LEN = len(tokens)
+x = torch.tensor(tokens, dtype=torch.long)
+x = x.unsqueeze(0).repeat(BATCH, 1) # (B, T)
+
+for i in tqdm.trange(MAX_LEN):
+    logits: torch.Tensor = model(x)
+    print(f'shape = {logits.shape}')
+    logits = logits[:, -1, :].squeeze(1)
+    assert logits.shape == (BATCH, Config.vocab_size)
+    probs = torch.softmax(logits, dim=-1)
+    (top_probs, top_idx) = torch.topk(probs, K)
+    next_idx = torch.multinomial(top_probs, 1) # sample 1 from top_k
+    assert next_idx.shape == (BATCH, 1)
+    next_token = torch.gather(top_idx, dim=1, index=next_idx)
+    assert next_token.shape == (BATCH, 1)
+    x = torch.cat((x, next_token), dim=1)[:, -Config.block_size:]
+
+response = enc.decode_batch(x.tolist())
+print(f'output = {response[0]}')
