f8410690bbdbffc8171a62cbd73e0ddd81224d11
Tianer Zhou <ezhoureal@gmail.com>
2025-04-18 14:07:27 +0800

    generalize for multi-head
    Signed-off-by: Tianer Zhou <ezhoureal@gmail.com>

--- rope.py
+++ rope.py
@@ -12,19 +12,21 @@ def naive_rope_cache(head_dim: int, seq_len: int, theta: float = 10000.0):
     assert ops.shape == (seq_len, head_dim // 2, 2)
     return ops
 
-cache = naive_rope_cache(4, 3)
+HEAD_DIM = 4
+HEADS = 2
+SEQ = 3
+cache = naive_rope_cache(HEAD_DIM, SEQ)
 print(cache)
-x = torch.randn(3, 1, 4) # (seq_len, heads, head_dim)
+x = torch.randn(SEQ, HEADS, HEAD_DIM)
 x = x.reshape(*x.shape[:-1], -1, 2) # group x along the head_dim
 
 print(x)
 
 cache = cache.unsqueeze(1)
-assert cache.shape == x.shape
 output = torch.stack([
     x[..., 0] * cache[..., 0] - x[..., 1] * cache[..., 1],
     x[..., 0] * cache[..., 1] + x[..., 1] * cache[..., 0]
 ], dim=-1)
 output = output.flatten(2)
-assert output.shape == (3, 1, 4)
+assert output.shape == (SEQ, HEADS, HEAD_DIM)
 print(output)
